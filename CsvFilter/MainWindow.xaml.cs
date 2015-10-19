3f6f0b3e85d9eceaec38b38345fb96c2a657857d
Paul Chu <lostyear@outlook.com>
2015-10-19 23:24:46 +0800

    Show Data Testing

--- CsvFilter/MainWindow.xaml.cs
+++ CsvFilter/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace CsvFilter
   public partial class MainWindow : Window
   {
     FileFilter ff = null;
+
     public MainWindow()
     {
       InitializeComponent();
@@ -29,16 +30,73 @@ namespace CsvFilter
     private void Window_Loaded(object sender, RoutedEventArgs e)
     {
       this.Hide();
+
+
+      string sourceFilePath = null;
+
+      //sourceFilePath = getSourceFilePath();
+      sourceFilePath = @"Z:\Internet_Shared\SharedDoc\gwy\公务员职位csv.csv";
+
+      if (sourceFilePath == null)
+      {
+        Application.Current.Shutdown();
+      }
+      else
+      {
+        ff = new FileFilter(sourceFilePath);
+        this.Show();
+      }
+    }
+
+    private void btnApply_Click(object sender, RoutedEventArgs e)
+    {
+      string[][] aa = new string[][] {
+        new string[] {"1","2","3"},
+        new string[] {"4","5","6"},
+        new string[] {"7","8","9"}
+      };
+      bindingArry(aa);
+    }
+
+    private string getSourceFilePath()
+    {
       FileOpener fo = new FileOpener();
       fo.ShowDialog();
       if (fo.DialogResult == true)
       {
-        this.Show();
-        ff = new FileFilter(fo.FilePath);
+        return fo.FilePath;
       }
       else
       {
-        Application.Current.Shutdown();
+        return null;
+      }
+
+    }
+
+    private void bindingArry(string[][] source)
+    {
+      Brush blackBrush = new SolidColorBrush(Colors.Black);
+      Brush grayBrush = new SolidColorBrush(Colors.LightBlue);
+      Thickness borderThickness = new Thickness(1);
+
+      foreach (string[] line in source)
+      {
+        ListViewItem lvi = new ListViewItem();
+        StackPanel sp = new StackPanel();
+        sp.Orientation = Orientation.Horizontal;
+        
+
+        foreach (string item in line)
+        {
+          Label itemLabel = new Label();
+          itemLabel.Background = grayBrush;
+          itemLabel.BorderBrush = blackBrush;
+          itemLabel.BorderThickness = borderThickness;
+          itemLabel.Content = item;
+          sp.Children.Add(itemLabel);
+        }
+        lvi.Content = sp;
+        lvTable.Items.Add(lvi);
       }
     }
 
